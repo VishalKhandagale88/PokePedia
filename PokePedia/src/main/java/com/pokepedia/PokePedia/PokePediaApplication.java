09667ae309391ed68d18a7a2767707fa317b0b39
vishal <vishal88kh@gmail.com>
2023-10-20 19:53:39 +0530

    added spring boot project

---
+++ PokePedia/src/main/java/com/pokepedia/PokePedia/PokePediaApplication.java
@@ -0,0 +1,13 @@
+package com.pokepedia.PokePedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokePediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokePediaApplication.class, args);
+	}
+
+}
---
+++ PokePedia/src/test/java/com/pokepedia/PokePedia/PokePediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokepedia.PokePedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokePediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
